55647831708bb4ac876ea61589ad4a1d746cbeda
zitonglu <910109610@qq.com>
2017-01-10 14:23:11 +0800

    admin on table

--- lh-admin/function/createdb.php
+++ lh-admin/function/createdb.php
@@ -3,16 +3,16 @@ echo '<p>开始创建数据库表</p>';
 
 function setup_createTable(){
 	global $dataBase,$adminInf;
-	$conn = mysql_connect($dataBase['dbhost'], $dataBase['dbuser'], $dataBase['dbpass']);
-	if(!$conn){
-        echo '<p class="text-danger">无法链接数据库,请检查填写是否正确</p>'.mysql_error();
-        return;
-    }else{
+	try {
+    	$dsn = 'mysql:host='.$dataBase['dbhost'].';dbname='.$dataBase['dbname'];
+    	$conn = new PDO($dsn,$dataBase['dbuser'],$dataBase['dbpass']);
     	echo '<p>链接数据库正常.....</p>';
+    } catch (PDOException $e) {
+        echo '<p class="text-danger text-center">Error!: ' . $e->getMessage() . '</p>';
+        echo '<p class="text-danger text-center">无法链接数据库,请检查填写是否正确</p>';
     }
-
-	mysql_select_db($dataBase['dbname'], $conn);
-
+    $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+    // 创建user表
 	$tableName = $dataBase['dbprefix'].'user';
 	$sql = 'CREATE TABLE '.$tableName.'(
 			ID int NOT NULL AUTO_INCREMENT primary key, 
@@ -20,22 +20,23 @@ function setup_createTable(){
 			user_pass varchar(30) NOT NULL,
 			user_name varchar(30) NOT NULL DEFAULT "",
 			user_email tinytext NOT NULL DEFAULT "",
-			user_tel varchar(11) NOT NULL DEFAULT "")';
-	if(mysql_query($sql,$conn)){
+			user_tel varchar(11) NOT NULL DEFAULT ""
+			)';
+	try{
+		$conn->exec($sql);
 		echo '<p>创建'.$tableName.'表，正常......</p>';
-	}else{
-		echo '<p class="text-danger">'.mysql_error().'</p>';
+	}catch(PDOException $e){
+		echo '<p class="text-danger">'.$e->getMessage().'</p>';
 	}
+	// 控制登录名不可重复
+	$sql = "ALTER TABLE `".$tableName."` ADD UNIQUE(`user_login`)";
+	$conn->exec($sql);
+	// 创建管理员信息
+	$sql = "insert into ".$tableName." (user_login,user_pass,user_email) values ('".$adminInf['admin']."','".$adminInf['password']."','".$adminInf['email']."')";
+	$conn->exec($sql);
+	echo '<p>插入管理员信息成功......</p>';
 
-	$sql = 'INSERT INTO '.$tableName.'(user_login,user_pass,user_email) VALUES('.$adminInf["admin"].','.$adminInf["password"].','.$adminInf["email"].')';
-	if(mysql_query($sql,$conn)){
-		echo '<p>管理员信息创建成功......</p>';
-	}else{
-		echo '<p class="text-danger">'.mysql_error().'</p>';
-	};
-	echo $sql;
-
-	mysql_close($conn);
+	$conn = null;
 }
 
 setup_createTable();
