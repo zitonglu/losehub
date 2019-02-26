3eca0f131aa126e7a9f4a8bf2177384e3eebf9d8
xie <910109610@qq.com>
2019-02-26 17:25:20 +0800

    @

--- lh-admin/function/base.php
+++ lh-admin/function/base.php
@@ -9,16 +9,16 @@
  */
 //版本号
 define('LH_VERSION_MAJOR','1');
-define('LH_VERSION_RSS','0');
-define('LH_VERSION_WEB','0');
-define('LH_VERSION_EDIT','0');
-define('LH_VERSION',LH_VERSION_MAJOR.'.'.LH_VERSION_RSS.LH_VERSION_WEB.LH_VERSION_EDIT);
+define('LH_VERSION_MODEL','0');
+define('LH_VERSION_VIEW','0');
+define('LH_VERSION_CONTROLLER','0');
+define('LH_VERSION',LH_VERSION_MAJOR.'.'.LH_VERSION_MODEL.LH_VERSION_VIEW.LH_VERSION_CONTROLLER);
 
 //安装地址
 defined('LH_PATH') || define('LH_PATH', rtrim(str_replace('\\', '/', realpath(dirname(__FILE__) . '/../../')), '/') . '/');
 
 //获取数据库信息
-if (file(LH_PATH.'lh-admin/database.php')){
+if (@file(LH_PATH.'lh-admin/database.php')){
 	$GLOBALS['LH_dataBase'] = require LH_PATH.'lh-admin/database.php';
 }
 
