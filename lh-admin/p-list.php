6f61e154460f6806ec974542aa3265cbc5efaa52
xie <910109610@qq.com>
2019-03-28 08:06:19 +0800

    list style

--- lh-admin/p-list.php
+++ lh-admin/p-list.php
@@ -24,14 +24,14 @@ foreach ($p_list as $p_lists) {
 	$echo .= '<tbody><tr>';
 	$echo .= '<th scope="row">'.$p_lists['id'].'</th>';
 	$echo .= '<td>'.get_type_name($p_lists['p_type_code']).'</td>';
-	$echo .= '<td>'.$p_lists['p_contect'].'</td>';
+	$echo .= '<td class="p-contect"><a href="#" class="ico"><span class="glyphicon glyphicon-link" aria-hidden="true"></span></a><a href="edit.php?id='.$p_lists['id'].'&&return=plist" title="'.$p_lists['p_contect'].'"> '.$p_lists['p_contect'].'</a></td>';
 	$echo .= '<td>'.get_state_name($p_lists['p_state_code']).'</td>';
 	$echo .= '<td>'.get_state_name($p_lists['p_c_state_code']).'</td>';
 	$echo .= '<td>'.substr($p_lists['p_datetime'],0,10).'</td>';
 	if ($p_lists['p_a_id'] == 1) {
-		$echo .= '<td>无</td>';
+		$echo .= '<td class="text-right">无</td>';
 	}else{
-		$echo .= '<td>'.$p_lists['p_a_id'].'- <span class="glyphicon glyphicon-sort-by-order" aria-hidden="true"></span> '.$p_lists['p_order'].'</td>';
+		$echo .= '<td class="text-right">'.$p_lists['p_a_id'].'- <span class="glyphicon glyphicon-sort-by-order" aria-hidden="true"></span> '.$p_lists['p_order'].'</td>';
 	}
 	$echo .= '<td><a href="edit.php?id='.$p_lists['id'].'&&return=plist" title="编辑"><span class="glyphicon glyphicon-pencil" aria-hidden="true"></span></a> ';
 	$echo .= '<span class="glyphicon glyphicon-remove" aria-hidden="true"></span></td>';
@@ -42,7 +42,7 @@ include('header.php');
 include('nav.php');
 ?>
 <div class="container p-list">
-	<table class="table table-striped table-hover">
+	<table class="table table-striped table-hover list-table">
 		<caption><span class="glyphicon glyphicon-list-alt" aria-hidden="true"></span> 段落列表</caption>
 		<thead>
 			<tr>
@@ -51,8 +51,8 @@ include('nav.php');
 				<th class="p-contect">内容</th>
 				<th>状态</th>
 				<th>评论</th>
-				<th>发布时间</th>
-				<th>归属长文</th>
+				<th>时间</th>
+				<th class="text-right">归属</th>
 				<th></th>
 			</tr>
 		</thead>
