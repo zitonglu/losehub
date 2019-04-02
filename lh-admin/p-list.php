ceddecb023182af15efb9c32f18df3e7b981949e
xie <910109610@qq.com>
2019-04-02 14:55:41 +0800

    page

--- lh-admin/p-list.php
+++ lh-admin/p-list.php
@@ -4,7 +4,7 @@
  * @copyright LoseHub
  * @author 紫铜炉 910109610@QQ.com
  * @global base.php($dbn;),$lh
- * @version 2019-3-27
+ * @version 2019-4-2
  * 
  * @return none
  */
@@ -12,8 +12,8 @@ require_once('function/base.php');
 require_once('function/authorize.php');
 
 $echo = '';
-$total = 1;
-// 获取总行数
+// $total = 1;
+// 获取翻页相关信息
 if (isset($_GET['listselect'])) {
 	$results_per_page = (int)$_GET['listselect'];// 翻页行数
 	$query = MySQL_options_change('row_number',$results_per_page,LH_DB_PREFIX.'options');
@@ -70,11 +70,23 @@ if (isset($_GET['orderby'])) {
 	}
 	$search_query .= $order;
 }
+$count_query = preg_replace('/\*/','count(*)',$search_query);//用于查询总行数
 
-echo $search_query;
+$search_query .= ' LIMIT ';
+if($cur_page > 1){
+	$i = (int)$results_per_page*($cur_page-1);
+	$search_query .= $i.',';
+}
+$search_query .= $results_per_page;
+// echo $search_query;
 $result = $dbn->prepare($search_query);
 $result->execute();
 $p_list = $result->fetchAll();
+
+$count = $dbn->query($count_query);
+$counts = $count->fetch();
+$total = $counts[0];// 获取总行数
+$num_page = ceil($total/$results_per_page);// 分页数
 
 foreach ($p_list as $p_lists) {
 	$echo .= '<tr>';
@@ -92,10 +104,7 @@ foreach ($p_list as $p_lists) {
 	$echo .= '<td><a href="edit.php?id='.$p_lists['id'].'&return=plist" title="编辑"><span class="glyphicon glyphicon-pencil" aria-hidden="true"></span></a> ';
 	$echo .= '<a href="deletedb.php?delid='.$p_lists['id'].'&return=plist" title="删除"><code><span class="glyphicon glyphicon-remove" aria-hidden="true"></span></code></a></td>';
 	$echo .= '</tr>';
-
-	$total ++;// 获取总行数-这是错的哦，要更换！
 }
-$num_page = ceil($total/$results_per_page);
 
 include('header.php');
 include('nav.php');
@@ -138,26 +147,26 @@ include('nav.php');
 	<div class="col-sm-5">
 	<nav aria-label="Page navigation">
 		<ul class="pagination">
-			<?php 
+			<?php
+			// Previous前一页
 			if( $cur_page > 1){
-				$page_link .= '<li><a href="#">1</a></li>';				
+				$page_link .= '<li><a href="'.changeURLGet('page',$cur_page - 1).'" aria-label="Previous"><span aria-hidden="true">&laquo;</span></a></li>';
 			}else{
-				$page_link .= '<li><a href="" aria-label="Previous">';
-				$page_link .= '<span aria-hidden="true">&laquo;</span>';
-				$page_link .= '</a></li>';
+				$page_link .= '<li class="disabled"><a href="#" aria-label="Previous"><span aria-hidden="true">&laquo;</span></a></li>';
 			}
-
+			// 循环页数
 			for ($i=1; $i <= $num_page; $i++) { 
-				if ($num_page == $i) {
-					$page_link .= ''.$i;
+				if ($cur_page == $i) {
+					$page_link .= '<li class="active"><a href="'.changeURLGet('page',$cur_page).'">'.$cur_page.'</a></li>';
 				}else{
-					$page_link .= '<li><a href="'.$_SERVER['PHP_SELF'].'?page=2">1</a></li>';
+					$page_link .= '<li><a href="'.changeURLGet('page',$i).'">'.$i.'</a></li>';
 				}
 			}
-			if ($cur_page < $num_page) {
-				$page_link .= '<li><a href="">2</a></li>';
+			// Next下一页
+			if ($cur_page < $num_page-1){
+				$page_link .= '<li><a href="'.changeURLGet('page',$cur_page + 1).'" aria-label="Next"><span aria-hidden="true">&raquo;</span></a></li>';
 			}else{
-				$page_link .= '<li><a href="#" aria-label="Next"><span aria-hidden="true">&raquo;</span></a></li>';
+				$page_link .= '<li class="disabled"><a href="#" aria-label="Next"><span aria-hidden="true">&raquo;</span></a></li>';
 			}
 			echo $page_link;
 			?>
