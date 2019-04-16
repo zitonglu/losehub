3f2e8ace63b648e0671adb770e1da82c1dcff694
xie <910109610@qq.com>
2019-04-16 16:35:50 +0800

    add p bug

--- lh-admin/function/edit-p.php
+++ lh-admin/function/edit-p.php
@@ -22,19 +22,19 @@ if (isset($_POST['send'])) {
 	if (isset($_POST['id']) && $_POST['id'] != '') {
 		$query = "replace into ".LH_DB_PREFIX.'paragraphs'." (";
 		$query .= "`p_contect`,`p_state_code`,`p_type_code`,`p_c_state_code`,`id`,`p_order`";
-		$query .= ") values (";
-		$query .= "'".$_POST['textarea']."','".$_POST['state']."','".$_POST['type']."','".$p_c_state_code."'".",".$_POST['id'].",".$_POST['p_order'];
+		if (isset($_POST['Aid'])) {//传递归属长文
+				$query .= ",`p_a_id`) values (";
+				$query .= "'".$_POST['textarea']."','P','P','P',".$_POST['id'].",".$_POST['p_order'].",".$_POST['Aid'];
+			}else{
+			$query .= ") values (";
+			$query .= "'".$_POST['textarea']."','".$_POST['state']."','".$_POST['type']."','".$p_c_state_code."'".",".$_POST['id'].",".$_POST['p_order'];
+		}
 		$query .= ")";
 	}else{
 		$query = "insert into ".LH_DB_PREFIX.'paragraphs'." (";
 		$query .= "`p_contect`,`p_state_code`,`p_type_code`,`p_c_state_code`,`p_order`";
-			if (isset($_POST['Aid'])) {//传递归属长文
-				$query .= ",`p_a_id`) values (";
-				$query .= "'".$_POST['textarea']."','P','P','P',".$_POST['p_order'].",".$_POST['Aid'];
-			}else{
-				$query .= ") values (";
-				$query .= "'".$_POST['textarea']."','".$_POST['state']."','".$_POST['type']."','".$p_c_state_code."',".$_POST['p_order'];
-		}
+		$query .= ") values (";
+		$query .= "'".$_POST['textarea']."','".$_POST['state']."','".$_POST['type']."','".$p_c_state_code."',".$_POST['p_order'];
 		$query .= ")";
 	}
 	// echo $query;
